a7f881d75daca2422d4ee71dfa09ee70c6a5d703
Andriana008 <andriana080999@gmail.com>
2018-04-06 16:12:40 +0300

    Add Sort and Find function

--- Task5/Task5/Program.cs
+++ Task5/Task5/Program.cs
@@ -8,6 +8,15 @@ namespace Task5
 {    
     class Program
     {
+        static List<IContact> CloneList(List<IContact> contacts)
+        {
+            List<IContact> cloned = new List<IContact>(contacts.Count);
+            for (int i = 0; i < contacts.Count(); i++)
+            {
+                cloned.Add((IContact)contacts[i].Clone());
+            }
+            return cloned;
+        }
         static void Main(string[] args)
         {
             try
@@ -22,10 +31,31 @@ namespace Task5
                 p.Input();
                 MailContact m = new MailContact();
                 m.Input();
-
+                contacts.Add(p);
+                contacts.Add(m);
                 for (int i = 0; i < contacts.Count(); i++)
                 {
                     contacts[i].Output();
+                }
+
+                Console.WriteLine("Sort by name: ");
+                List<IContact> sortedContacts = CloneList(contacts);
+                sortedContacts.Sort();
+                for (int i = 0; i < sortedContacts.Count(); i++)
+                {
+                    sortedContacts[i].Output();
+                }
+
+                Console.WriteLine("Get phone number by name");
+                Console.WriteLine("Enter name: ");
+                string vname = Console.ReadLine();
+                if (!contacts.Exists(x => x.Name.Contains(vname)))
+                {
+                    Console.WriteLine("Name doesn't exist in this score");
+                }
+                else
+                {
+                    Console.WriteLine("Number is {0}", contacts.Find(x => x.Name.Contains(vname)).Info);
                 }
             }
             catch(ArgumentException e)
